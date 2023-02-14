40711c49a81882ce0d78d8efd31d5078527d94e2
MDDevelopers <125076374+MDDevelopers@users.noreply.github.com>
2023-02-14 15:54:59 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -101,9 +101,9 @@ const handleSubmit = async (e) => {
 
     if (response.ok) {
         const data = await response.json();
-        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+        const parsedData = data.bot.trim(); 
 
-        typeText(messageDiv, parsedData)
+        typeText(messageDiv, parsedData);
     } else {
         const err = await response.text()
 

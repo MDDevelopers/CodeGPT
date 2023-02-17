205c65a1ba2facaf2ee373a5c4ed43be7c527e5a
MDDevelopers <125076374+MDDevelopers@users.noreply.github.com>
2023-02-17 12:47:07 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch("https://codegpt-o0vx.onrender.com/", {
+    const response = await fetch("https://codegpt-5d1h.onrender.com", {
         method: 'POST',
         headers: {
             'Content-Type': "application/json",

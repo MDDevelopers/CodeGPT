67937aa26eb73b5437b2b1dc3f63eccb5fecd723
Mohammed Sabeel K <125076374+MDDevelopers@users.noreply.github.com>
2023-06-28 12:32:23 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,25 +18,32 @@ app.get('/', async (req, res) =>{
 })
 
 
-app.post('/', async (req,res) =>{
-    try {
-        const prompt = req.body.prompt;
+app.post('/', async (req, res) => {
+  try {
+    const { prompt } = req.body;
 
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${prompt}`,
-            temperature: 0.7,
-            max_tokens: 3000,
-            top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0,
-        });
-        res.status(200).send({
-            bot: response.data.choices[0].text
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ error })
+    if (!prompt || typeof prompt !== 'string') {
+      res.status(400).send({ error: 'Invalid or missing prompt' });
+      return;
     }
-})
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0.7,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: 'An error occurred' });
+  }
+});
+
 app.listen(5000,() => console.log('Server is Running on port http://localhost:5000'));
